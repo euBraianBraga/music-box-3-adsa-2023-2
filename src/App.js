a75338af660edbaeda81eedace69844b5a601aa2
euBraianBraga <braian.braga@bandtec.com.br>
2023-09-26 12:42:15 -0300

    Primeira aula

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import api from "./api";
+import { useState } from "react";
 function App() {
+  const [musicas, setMusicas] = useState([]);
+  function listar() {
+    api
+      .get()
+      .then((respostaObtida) => {
+        console.log(respostaObtida.data);
+        setMusicas(respostaObtida.data);
+      })
+      .catch((erroOcorrido) => {
+        console.log(erroOcorrido);
+      });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Titulo</h1>
+      <button onClick={listar}>Listar</button>
+      {musicas.map((musica) => (
+        <div key={musica.id}>
+          <h1>{musica.nome}</h1>
+          <h1>{musica.artista}</h1>
+          <br />
+        </div>
+      ))}
+    </>
   );
 }
-
 export default App;
