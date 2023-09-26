a476c6daaf76e9a244e4be6695b594aaa704d5c6
euBraianBraga <braian.braga@bandtec.com.br>
2023-09-26 15:03:56 -0300

    Comentando a aula de 25/09

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import api from "./api";
-import { useState } from "react";
+// Importa a instância "api" que você criou em "./api", que permite fazer solicitações HTTP para sua API.
+// import api from "./api";
+
+// Define o componente "App", que é o componente principal da sua aplicação.
 function App() {
-  const [musicas, setMusicas] = useState([]);
-  function listar() {
-    api
-      .get()
-      .then((respostaObtida) => {
-        console.log(respostaObtida.data);
-        setMusicas(respostaObtida.data);
-      })
-      .catch((erroOcorrido) => {
-        console.log(erroOcorrido);
-      });
-  }
   return (
     <>
       <h1>Titulo</h1>
-      <button onClick={listar}>Listar</button>
-      {musicas.map((musica) => (
-        <div key={musica.id}>
-          <h1>{musica.nome}</h1>
-          <h1>{musica.artista}</h1>
-          <br />
-        </div>
-      ))}
     </>
   );
 }
+
+// Exporta o componente "App" para que ele possa ser importado e usado em outros lugares do seu código.
 export default App;
+
+/*
+
+Importamos a instância "api" que você criou em "./api". 
+Isso permite que você acesse e utilize essa instância para fazer solicitações HTTP à 
+sua API dentro do componente "App" ou em qualquer outro lugar do seu código.
+
+Definimos o componente "App". Este é o componente principal da sua aplicação React, 
+e ele retorna o JSX que será renderizado na página. No exemplo fornecido, ele simplesmente renderiza um título <h1>Titulo</h1>.
+
+Exportamos o componente "App" usando export default App;. Isso permite que outros arquivos do seu projeto 
+importem e usem o componente "App" como necessário. Por exemplo, você pode usá-lo como um componente raiz
+ em sua aplicação, como você fez na parte anterior do códigou
+
+*/
